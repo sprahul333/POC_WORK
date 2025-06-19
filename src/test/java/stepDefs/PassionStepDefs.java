4f1590eb397b3779375305359e4fa57f00a039e1
sprahul333 <sprahul333@gmail.com>
2025-06-19 11:17:58 +0530

    Modified the code

--- src/test/java/stepDefs/PassionStepDefs.java
+++ src/test/java/stepDefs/PassionStepDefs.java
@@ -53,5 +53,6 @@ public class PassionStepDefs extends ReusableLibrary
 
     @Then("Approve the Pawn Application for disbursement using SBG Number {string}")
     public void approveThePawnApplicationForDisbursementUsingSBGNumber(String sbgNumber) {
+        passionBusiness.approveTheRequestAtThirdLevel();
     }
 }
